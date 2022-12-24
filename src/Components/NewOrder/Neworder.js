7b7226ce04c3ec01c98d27ff5f9c7c7964e57d23
nguyendinhtu2002 <nguyendinhtu11022002@gmail.com>
2022-12-24 08:47:50 +0700

    update

--- src/Components/NewOrder/Neworder.js
+++ src/Components/NewOrder/Neworder.js
@@ -45,6 +45,51 @@ function Neworder() {
                             </select>
 
                         </div>
+                        <div className='mb-3 lg:col-span-3 ' >
+                            <label className=' mb-[0.5rem] text-[1.05rem] font-medium color-[#3f4254] inline-block '>
+                                Service
+                            </label>
+                            {/* <select onChange={(e) => setCategory(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                {
+                                    redirect === "" ? <option></option> : ""
+                                }
+                                {
+                                    Category.map((items, index) =>
+                                        <option key={index} value={items}>{items}</option>
+
+                                    )
+                                }
+
+                            </select> */}
+                            <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option></option> : ""
+                            </select>
+
+                        </div>
+                        <div className='mb-3 lg:col-span-3 ' >
+                            <label className=' mb-[0.5rem] text-[1.05rem] font-medium color-[#3f4254] inline-block '>
+                                Link
+                            </label>
+                            <div className='input-group input-group-solid mb-2'>
+                                <input type="text" id="default-input"  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+
+                            </div>
+
+
+                        </div>
+                        <div className='mb-3 lg:col-span-3 ' >
+                            <label className=' mb-[0.5rem] text-[1.05rem] font-medium color-[#3f4254] inline-block '>
+                                Quantity
+                            </label>
+                            <div className='input-group input-group-solid mb-2'>
+                                <input type="text" id="default-input" placeholder="From 500 to 2000" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+
+                            </div>
+
+
+                        </div>
+                       
+
                     </div>
                 </div>
             </div>
